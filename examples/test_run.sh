0dfcf1cd5333a4ae8c45ed3314e3515e864e7bc6
Matthew Farrell <matthew.farrell.235@gmail.com>
2025-03-09 16:35:49 +0900

    running without errors.

--- examples/test_run.sh
+++ examples/test_run.sh
@@ -1,4 +1,7 @@
 # On branch small-fixes
 python phc_mjx/run.py env=env_im env.motion_file=data/amass/amass_isaac_standing_upright_slim.pkl exp_name=im_obsv2_1 env.self_obs_v=2 headless=True test=False num_threads=1 learning.min_batch_size=256
 
+# This one is the main one I was testing (commit a2e53da)
 py phc_mjx/run.py env=env_im env.motion_file=data/sim_data.pkl exp_name=im_obsv2_1 env.self_obs_v=2 headless=True test=False num_threads=1 learning.min_batch_size=256 env.motion_file_type='mujoco' env.model_file="data/mujoco/humanoid_and_tennis.xml" robot.humanoid_type="smpl" robot.xml_file=""data/mujoco/humanoid_and_tennis.xml env.reset_bodies_file="" robot.use_smpl_data="False"
+
+python phc_mjx/run.py env=env_im env.motion_file=data/amass/amass_isaac_standing_upright_slim.pkl exp_name=im_obsv2_1 env.self_obs_v=2 headless=True test=False num_threads=1 learning.min_batch_size=256
